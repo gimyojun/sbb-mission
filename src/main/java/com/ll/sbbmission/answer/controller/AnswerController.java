ccc15d38bc6f97597ac0dcc42b5441cc3027f018
gimyojun <gywnsdl5623@gmail.com>
2023-11-29 22:01:33 +0900

    글 등록, 댓글 등록 Form구현

--- src/main/java/com/ll/sbbmission/answer/controller/AnswerController.java
+++ src/main/java/com/ll/sbbmission/answer/controller/AnswerController.java
@@ -1,15 +1,17 @@
 package com.ll.sbbmission.answer.controller;
 
+import com.ll.sbbmission.answer.form.AnswerForm;
 import com.ll.sbbmission.answer.service.AnswerService;
 import com.ll.sbbmission.question.entity.Question;
 import com.ll.sbbmission.question.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,10 +24,16 @@ public class AnswerController {
     public String createAnswer(
             Model model,
             @PathVariable("id") Integer id,
-            @RequestParam String content
+            @Valid AnswerForm answerForm,
+            BindingResult bindingResult
             ) {
         Question question = this.questionService.getQuestion(id);
-        answerService.create(question, content);
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("question", question);
+            return "/question/detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
 
         return String.format("redirect:/question/detail/%s", id);
     }
