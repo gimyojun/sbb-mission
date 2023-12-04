fc045b42e16a74bf697b65acd9cbddebe7027321
gimyojun <gywnsdl5623@gmail.com>
2023-12-05 04:47:03 +0900

    회원가입을 위한 UserController, 엔티티, 레포지터리 생성

---
+++ src/main/java/com/ll/sbbmission/user/UserService.java
@@ -0,0 +1,20 @@
+package com.ll.sbbmission.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private  final PasswordEncoder passwordEncoder;
+    public SiteUser create(String username, String email, String password){
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setEmail(email);
+        this.userRepository.save(user);
+        return user;
+    }
+}
